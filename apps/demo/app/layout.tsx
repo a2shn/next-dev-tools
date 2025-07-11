b155d287ed50e1a936de3edf2529e5f6cea31d98
a2shn <209373144+a2shn@users.noreply.github.com>
2025-07-11 13:46:19 +0000

    refactor: deleted deprecated code

--- apps/demo/app/layout.tsx
+++ apps/demo/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { NextDevTools } from '@next-dev-tools/client';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -28,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NextDevTools>{children}</NextDevTools>
+        {children}
       </body>
     </html>
   );
